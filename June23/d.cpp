a8e68a49bef937c7cedcf6919db2c201374be982
Suraj Patel <real4suraj@gmail.com>
2020-06-23 19:15:08 +0530

    June 23 2020

---
+++ June23/d.cpp
@@ -0,0 +1,92 @@
+//+LNCT++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+------------------Lord Escanor : Sin Of Pride----------------------+//
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++BHOPAL+//
+
+#include <bits/stdc++.h>
+
+#define fo(i, n) for (int i = 0; i < int(n); i++)
+#define of(i, n) for (int i = int(n) - 1; i >= 0; i--)
+#define Fo(i, l, r) for (int i = int(l); i < int(r); i++)
+#define correct(x, y, n, m) (0 <= (x) && (x) < (n) && 0 <= (y) && (y) < (m))
+#define all(a) (a).begin(), (a).end()
+#define sz(a) int((a).size())
+#define pb(a) push_back(a)
+#define mp(x, y) make_pair((x), (y))
+#define x first
+#define y second
+
+#define endl '\n'
+#define deb(x) cout << #x << " " << x << endl;
+
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+
+template<typename X> inline X abs(const X& a) { return a < 0 ? -a : a; }
+template<typename X> inline X sqr(const X& a) { return a * a; }
+template<typename... T>
+void read(T&... args) {
+    ((cin >> args), ...);
+}
+template<typename... T>
+void write(T&&... args) {
+    ((cout << args << " "), ...);
+}
+
+const int INF = int(1e9);
+const ll INF64 = ll(1e18);
+const ld EPS = 1e-9, PI = 3.1415926535897932384626433832795;
+
+vi pos[26];
+void solve() {
+    int n, m;
+    string s;
+    cin >> n >> s;
+    for(int i = 0; i < n; ++i){
+        pos[s[i] - 'a'].pb(i + 1);
+    }
+    cin >> m;
+    for(int i = 0; i < m; ++i){
+        string t;
+        cin >> t;
+        vi cnt(26);
+        for(auto &c : t){
+            ++cnt[c - 'a'];
+        }
+        int ans = -1;
+        for(int j = 0; j < 26; ++j){
+            if(cnt[j] > 0){
+                ans = max(ans, pos[j][cnt[j] - 1]);
+            }
+        }
+        cout << ans << endl;
+    }
+
+}
+
+int main() {
+    fastIO;
+//#ifdef LOCAL
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+//#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+#ifdef LOCAL
+    cout<<"\nTime Elapsed: " << 1.0*clock() / CLOCKS_PER_SEC << " sec\n";
+#endif
+
+    return 0;
+}
+
+
+
